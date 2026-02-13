5965f0dce003201131cdd2f8172d390381ffd156
Yazmin Gutierrez Hernandez <230447@utxicotepec.edu.mx>
2026-02-13 00:31:45 -0600

    Actualizacion de readme
    Se actualizo el readme de la carpeta 02_RUTINAS_SQL

---
+++ PRACTICA 05/02_RUTINAS_SQl/funciones.sql
@@ -0,0 +1,31 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `fn_apellido_real`() RETURNS varchar(50) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+    RETURN ELT(FLOOR(1 + RAND()*10),
+        'Garcia','Lopez','Martinez','Hernandez','Perez',
+        'Gomez','Torres','Ramirez','Flores','Sanchez'
+    );
+END
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `fn_estatus_medico`(p_estatus VARCHAR(50)) RETURNS varchar(50) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+    
+    IF p_estatus IS NOT NULL AND 
+       p_estatus IN ('Estable',
+                     'En Observacion',
+                     'Coma',
+                     'Estado Vegetativo',
+                     'Cuidados Paliativos') THEN
+        RETURN p_estatus;
+    END IF;
+
+    
+    RETURN ELT(FLOOR(1 + RAND()*5),
+        'Estable',
+        'En Observacion',
+        'Coma',
+        'Estado Vegetativo',
+        'Cuidados Paliativos'
+    );
+END
