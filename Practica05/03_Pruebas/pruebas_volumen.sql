6b34aa7cbac20fbc604013ccca61dcdda5222970
Ktmich2095 <230881@utxicotepec.edu.mx>
2026-02-13 00:11:04 -0600

    Implementación de llamados
    pruebas de volumen para 10, 100, 1000, 10000 y 1000000 de pacientes

--- Practica05/03_Pruebas/pruebas_volumen.sql
+++ Practica05/03_Pruebas/pruebas_volumen.sql
@@ -2,3 +2,13 @@
 
 
 CALL sp_poblar_pacientes(1,NULL,NULL,NULL,NULL);
+
+CALL sp_poblar_pacientes(10,NULL,NULL,NULL,NULL);
+
+CALL sp_poblar_pacientes(100,NULL,NULL,NULL,NULL);
+
+CALL sp_poblar_pacientes(1000,NULL,NULL,NULL,NULL);
+
+CALL sp_poblar_pacientes(10000,NULL,NULL,NULL,NULL);
+
+CALL sp_poblar_pacientes(1000000,NULL,NULL,NULL,NULL);
